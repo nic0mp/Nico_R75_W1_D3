8293f2864ad63612141921536ec6e1342f14bd14
Nico Pinguel <nico.mp215@gmail.com>
2021-10-20 20:20:59 -0500

    commit

---
+++ circ_mod.py
@@ -0,0 +1,7 @@
+def circ_m():
+    pi = 3.14
+    radius= float(input("Enter radius: "))
+    circumference = 2*pi*radius
+    return circumference
+
+print(circ_m())
---
+++ sqfootage_mod.py
@@ -0,0 +1,8 @@
+def sqfootage():
+
+    length = float(input("Enter length: "))
+    width = float(input("Enter width: "))
+    area = length * width
+    return area
+    
+print(sqfootage())
